b241076ef739533209bf8f397dfda6239f694c37
Trishpat2007 <trishpat2007@gmail.com>
2024-08-13 09:40:40 -0700

    Update Program3.java
    Completed program

--- Program3.java
+++ Program3.java
@@ -3,12 +3,41 @@
 import java.util.Scanner; //This imports the console scanner that reads user input
 
 //Your code here:
-
-
-
+class Program3{
+    public static void main (String[] args){
+        //make a scanner
+        Scanner myScanner = new Scanner(System.in);
+        //create variables
+        int length = 0;
+        int width = 0; 
+        int area = 0; 
+        int perimeter = 0;
+        //ask user for input and store
+        System.out.println ("Enter Length: ");
+        length = myScanner.nextInt(); 
+        
+        System.out.println ("Enter Width: ");
+        width = myScanner.nextInt();
+        //calculate area and perimeter
+        area = (length*width) ; 
+        perimeter= (2*length + 2*width);
+        //show output 
+        System.out.println("Length is "+ length);
+        System.out.println("Width is " + width);
+        System.out.println("Area is " + area);
+        System.out.println("Perimeter is " + perimeter);
+    }
+}
 
 //Paste console output below:
 /*
-
+Enter Length: 
+10
+Enter Width: 
+10
+Length is 10
+Width is 10
+Area is 100
+Perimeter is 40
 
 */
